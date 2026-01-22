b16e9630f6ccb17b871ed5d3ad46b21a6ee1a4b2
Sean Martinez <185059685+Sean-Pers0nal@users.noreply.github.com>
2026-01-22 08:34:46 +0800

    Add files via upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-// 1. Import your new component
-import { PipesDemoComponent } from './pipes-demo/pipes-demo';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router'; // Add this import
+import { EmployeeService } from './employee';
+import { ProductsService } from './products';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    PipesDemoComponent
-  ],
+  imports: [CommonModule, RouterOutlet], // Add RouterOutlet here
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('mid-project');
+export class AppComponent implements OnInit { // Class name is AppComponent
+  public employees: any[] = [];
+  public products: any[] = [];
+
+  constructor(
+    private _employeeService: EmployeeService,
+    private _productsService: ProductsService
+  ) {}
+
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+    this.products = this._productsService.getProducts();
+  }
 }
